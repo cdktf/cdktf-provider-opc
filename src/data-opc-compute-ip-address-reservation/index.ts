c3ad771169f2a033fa6f477ae24a31ba295c734e
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-12-14 13:06:15 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-opc-compute-ip-address-reservation/index.ts
+++ src/data-opc-compute-ip-address-reservation/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/hashicorp/opc/1.4.1/docs/data-sources/compute_ip_address_reservation
 // generated from terraform resource schema
 
